5dfd8fa1455b18a664d1482bd5300ba4326388ac
nicrack09 <138498720+nicrack09@users.noreply.github.com>
2025-03-25 19:15:14 +0100

    Update player.py
    ya te vale nico

--- src/player.py
+++ src/player.py
@@ -1,6 +1,6 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
-
+import player_collider
 """
 self.health es la vida del player
 self.damage es el daño que hace el player
@@ -18,7 +18,7 @@ los valores de hp, dmg, spd, jmp y g son los valores iniciales de la clase Playe
 """
 
 class Player(FirstPersonController):
-    def __init__(self, hp, dmg, spd, jmp, g):
+    def __init__(self, hp, dmg, spd, jmp, g, r):
        super().__init__()
        self.health = hp
        self.damage = dmg
@@ -26,17 +26,19 @@ class Player(FirstPersonController):
        self.jump_height = jmp
        self.gravity = g
        self.jump_up_duration = 1
+       self.attack_range = r
        self.healthbar = Text(text=self.health, position=(-0.85, -0.4), scale=2, color=color.red)
-       
     def takeDamage(self, damage):
         self.health -= damage
         self.healthbar.text = self.health
-
+    def input(self,key):
+        if key== "left mouse down":
+            attack=player_collider.player_collider(self)
 
 
 class berserker(Player):
     def __init__(self):
-        super().__init__(150,30, 5, 3, 1.5)
+        super().__init__(150,30, 5, 3, 1, 10)
         
         
 
