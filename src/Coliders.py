a30dc8c1037c26a42adb564d1deb8ee37a280f25
nicrack09 <138498720+nicrack09@users.noreply.github.com>
2025-03-25 19:18:56 +0100

    Delete src/Coliders.py

--- src/Coliders.py
+++
@@ -1,9 +0,0 @@
-from ursina import *
-from time import *
-
-class Colider(Entity):
-    def __init__(self, damage, source, pposition, porientation, dimensions):
-        super().__init__(model = "square", color = color.white, position = pposition, rotation = porientation, scale=dimensions)
-        self.damage = damage
-        self.source = source
-
